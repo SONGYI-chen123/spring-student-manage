453fcb13c14e8fc6eade7ecc7080d52f5f21e0cb
SONGYI-chen123 <song1577660501@163.com>
2021-04-05 22:40:53 +0800

    feat: select partial attributes-2

--- src/main/java/com/example/demo/infrastructure/persistence/repository/JpaStudentRepository.java
+++ src/main/java/com/example/demo/infrastructure/persistence/repository/JpaStudentRepository.java
@@ -5,11 +5,11 @@ import com.example.demo.domain.student.entity.Student;
 import com.example.demo.domain.student.repository.StudentRepository;
 import com.example.demo.infrastructure.persistence.entity.QStudentPo;
 import com.example.demo.infrastructure.persistence.entity.StudentPo;
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.QBean;
 import com.querydsl.jpa.impl.JPAQuery;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.example.demo.infrastructure.persistence.assembler.StudentDataMapper.mapper;
 
@@ -21,18 +21,9 @@ public interface JpaStudentRepository extends StudentRepository,
 
     default List<Student> findAllStudents() {
         QStudentPo studentPo = QStudentPo.studentPo;
-        JPAQuery<Tuple> from = getJpaQueryFactory().select(studentPo.name, studentPo.age).from(studentPo);
+        QBean<Student> bean = Projections.bean(Student.class,studentPo.name,studentPo.age);
+        JPAQuery<Student> from = getJpaQueryFactory().select(bean).from(studentPo);
 
-        return from.fetch().stream().map(this::toStudent).collect(Collectors.toList());
-    }
-
-    default Student toStudent(Tuple tuple) {
-        return Student.builder()
-                .id(tuple.get(QStudentPo.studentPo.id))
-                .age(tuple.get(QStudentPo.studentPo.age))
-                .phoneNumber(tuple.get(QStudentPo.studentPo.phoneNumber))
-                .gender(tuple.get(QStudentPo.studentPo.gender))
-                .name(tuple.get(QStudentPo.studentPo.name))
-                .build();
+        return from.fetch();
     }
 }
